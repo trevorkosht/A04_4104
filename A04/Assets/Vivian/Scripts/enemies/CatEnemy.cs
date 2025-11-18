aa7029175ccbb52a0da5d216081ef622af34caa8
Vivian Wang <wang.15637@buckeyemail.osu.edu>
2025-11-18 15:07:17 -0500

    heiarchy chnages

---
+++ A04/Assets/Vivian/Scripts/enemies/CatEnemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatEnemy : BaseEnemy
+{
+    protected override void PerformAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
