a192338e16fc01c840bf16fa1d00bd807f4c2ca4
vivianwang15637 <wang.15637@buckeyemail.osu.edu>
2025-11-26 23:51:40 -0500

    branch name

--- A04/Assets/Trevor/Scripts/Spells/SpellDatabase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "SpellDatabase", menuName = "Spells/Database")]
-public class SpellDatabase : ScriptableObject
-{
-    public List<GridSpellSO> gridSpells;
-}
